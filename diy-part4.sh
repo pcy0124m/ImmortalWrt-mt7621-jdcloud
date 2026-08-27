a13f5452c25f82dbf893b41241eec059d9f39e66
pcy0124m <765266885@qq.com>
2026-08-27 10:14:14 +0800

    Fix sed command syntax in diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -2,12 +2,12 @@
 # 在 feeds 安装后、生成配置前执行
 
 # 改默认 LAN IP（默认 192.168.1.1，这里改成 192.168.31.1，按需改）
-# sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
+ sed -i 's/192.168.1.1/192.168.12.1/g' package/base-files/files/bin/config_generate
 
 # 关闭 IPv6（不需要可注释掉）
 # sed -i 's/ip6assign/#ip6assign/g' package/network/config/network/files/network.config 2>/dev/null || true
 
 # 把默认主题从 bootstrap 改成 argon
 sed -i 's#luci-static/bootstrap#luci-static/argon#g' \
-  feeds/luci/modules/luci-base/root/etc/config/luci 2>/dev/null || true
+feeds/luci/modules/luci-base/root/etc/config/luci 2>/dev/null || true
 
