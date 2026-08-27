6dcc6ba860fd296b176202daa63c192228db1803
pcy0124m <765266885@qq.com>
2026-08-27 10:09:37 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# 在 feeds 更新前执行。默认全部注释，需要时去掉 # 即可。
-# ⚠️ 25.12 已换 apk，第三方源是否适配需自行确认，加了不兼容的源会编译失败
+# ===== 在 feeds 更新前执行 =====
 
-# 科学上网（PassWall，第三方源）
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >> feeds.conf.default
-# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >> feeds.conf.default
+# --- 科学上网：OpenClash ---
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 
-# 或 OpenClash（第三方源）
-# echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+# --- Argon 主题（含主题配置插件）---
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
